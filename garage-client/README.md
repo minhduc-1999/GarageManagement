572d141b91135490ef5aed9d6c45e9b59091b494
Mercury <quocan.omg@gmail.com>
2021-05-16 23:45:11 +0700

    init front-end

---
+++ garage-client/README.md
@@ -0,0 +1,1 @@
+#### Depth Software Engineering Project
---
+++ garage-client/src/Layout/AppNav/NavItems.js
@@ -0,0 +1,144 @@
+export const MainNav = [
+    {
+        icon: 'pe-7s-rocket',
+        label: 'Dashboard Example',
+        to: '#/dashboards/basic',
+    },
+];
+export const ComponentsNav = [
+  
+    {
+        icon: 'pe-7s-graph3',
+        label: 'Report',
+        to: '#/tables/regular-tables',
+    },
+    {
+        icon: 'pe-7s-car',
+        label: 'Received Car',
+        to: '#/tables/regular-tables',
+    },
+    {
+        icon: 'pe-7s-news-paper',
+        label: 'Invoice',
+        to: '#/tables/regular-tables',
+    },
+    {
+        icon: 'pe-7s-users',
+        label: 'Customer',
+        to: '#/tables/regular-tables',
+    },
+    {
+        icon: 'pe-7s-tools',
+        label: 'Accessory',
+        content: [
+            {
+                label: 'Receipt',
+                to: '#/elements/buttons-standard',
+            },
+            {
+                label: 'Quotation',
+                to: '#/elements/dropdowns',
+
+            },
+            // {
+            //     label: 'Icons',
+            //     to: '#/elements/icons',
+            // },
+            // {
+            //     label: 'Badges',
+            //     to: '#/elements/badges-labels',
+            // },
+            // {
+            //     label: 'Cards',
+            //     to: '#/elements/cards',
+            // },
+            // {
+            //     label: 'List Groups',
+            //     to: '#/elements/list-group',
+            // },
+            // {
+            //     label: 'Navigation Menus',
+            //     to: '#/elements/navigation',
+            // },
+            // {
+            //     label: 'Utilities',
+            //     to: '#/elements/utilities',
+            // },
+        ],
+    },
+    {
+        icon: 'pe-7s-id',
+        label: 'Employee',
+        to: '#/tables/regular-tables',
+    },
+    // {
+    //     icon: 'pe-7s-car',
+    //     label: 'Components',
+    //     content: [
+    //         {
+    //             label: 'Tabs',
+    //             to: '#/components/tabs',
+    //         },
+    //         {
+    //             label: 'Notifications',
+    //             to: '#/components/notifications',
+    //         },
+    //         {
+    //             label: 'Modals',
+    //             to: '#/components/modals',
+    //         },
+    //         {
+    //             label: 'Progress Bar',
+    //             to: '#/components/progress-bar',
+    //         },
+    //         {
+    //             label: 'Tooltips & Popovers',
+    //             to: '#/components/tooltips-popovers',
+    //         },
+    //         {
+    //             label: 'Carousel',
+    //             to: '#/components/carousel',
+    //         },
+    //         {
+    //             label: 'Maps',
+    //             to: '#/components/maps',
+    //         },
+    //     ],
+    // },
+];
+// export const FormsNav = [
+//     {
+//         icon: 'pe-7s-light',
+//         label: 'Controls',
+//         to: '#/forms/controls',
+//     },
+//     {
+//         icon: 'pe-7s-eyedropper',
+//         label: 'Layouts',
+//         to: '#/forms/layouts',
+//     },
+//     {
+//         icon: 'pe-7s-pendrive',
+//         label: 'Validation',
+//         to: '#/forms/validation',
+//     },
+// ];
+export const WidgetsNav = [
+    {
+        icon: 'pe-7s-comment',
+        label: 'Repair Request',
+        to: '#/widgets/dashboard-boxes',
+    },
+    {
+        icon: 'pe-7s-info',
+        label: 'Car Status',
+        to: '#/widgets/dashboard-boxes',
+    },
+];
+// export const ChartsNav = [
+//     {
+//         icon: 'pe-7s-graph2',
+//         label: 'ChartJS',
+//         to: '#/charts/chartjs',
+//     },
+// ];
