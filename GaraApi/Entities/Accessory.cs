60d30eda00b3ab09f9bf0ec2489a1d7e0db6eddb
MinhDuc <minhduc9111999@gmail.com>
2021-06-09 11:57:23 +0700

    test api folder

--- GaraApi/Entities/Accessory.cs
+++ GaraApi/Entities/Accessory.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -9,23 +11,30 @@ namespace GaraApi.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
+        [BsonRequired]
+        [Required]
         public string Name { get; set; }
-
-        public string Brand { get; set; }
-
         public int Quantity { get; set; }
-
+        [BsonRequired]
+        [Required]
         public string Unit { get; set; }
-        public double ReceipPrice { get; set; }
+        public double ReceiptPrice { get; set; }
         public double IssuePrice { get; set; }
         [BsonRepresentation(BsonType.DateTime)]
         [BsonDateTimeOptions(DateOnly = true)]
+        [BsonIgnoreIfNull]
+        [BsonIgnoreIfDefault]
         public DateTime ExpiredDate { get; set; }
         [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRequired]
+        [Required]
         public string ProviderId { get; set; }
         [BsonRepresentation(BsonType.ObjectId)]
+        [BsonRequired]
+        [Required]
         public string AccessoryTypeId { get; set; }
+        [BsonIgnoreIfNull]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string Description { get; set; }
 
     }
--- GaraApi/Entities/AcessoryType.cs
+++ GaraApi/Entities/AcessoryType.cs
@@ -1,12 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace GaraApi.Entities{
-    public class AccessoryType{
+namespace GaraApi.Entities
+{
+    public class AccessoryType
+    {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id {get; set;}
+        public string Id { get; set; }
         [BsonRequired]
-        public string Name {get; set;}
+        [Required]
+        public string Name { get; set; }
     }
 }
