79ee0c21025c5cb4b402a44f18324387f682ee54
Mercury <quocan.omg@gmail.com>
2021-06-23 23:08:25 +0700

    edit user profile

--- gara-client/src/views/UserProfile.js
+++ gara-client/src/views/UserProfile.js
@@ -230,9 +230,8 @@ function UserProfile() {
                         className="avatar"
                         src={require("assets/img/deepLogo.png").default}
                       />
-                      <h5 className="title">Mike Andrew</h5>
                     </a>
-                    <p className="description">Ceo/Co-Founder</p>
+                    <p className="description">Garage's Founder</p>
                   </div>
                   <div className="card-description">
                     Do not be scared of the truth because we need to restart the
