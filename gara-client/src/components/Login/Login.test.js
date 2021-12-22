ef145d8aaf6c098566c03edbb1993e517c8e0a8d
minhduc <minhduc9111999@gmail.com>
2021-12-23 00:06:13 +0700

    test validate username

---
+++ gara-client/src/components/Login/Login.test.js
@@ -0,0 +1,21 @@
+import { ValidateUsername } from './Login'
+
+describe("Validate username", () => {
+    test.each([
+        [true, "username1"],
+        [false, "user"],
+        [false, ""],
+        [false, "usernameusernamelong1"],
+        [false, "user name"],
+        [false, "username_"],
+        [false, "_username"],
+        [false, ".username"],
+        [false, "username."],
+        [false, "user_.name."],
+        [false, "user._name."],
+        [false, "user..name."],
+    ])("Should return [%p] when enter [%s] as username", (expected, username) => {
+        const result = ValidateUsername(username)
+        expect(result).toEqual(expected)
+    })
+})
